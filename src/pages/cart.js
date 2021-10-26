0c849c893143e89bdc22e7927d0cb6bfa2824894
pmcg31 <seiffertrk@gmail.com>
2021-10-26 16:25:17 -0700

    Remove console logs

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -11,17 +11,15 @@ const CartIcon = () => {
 
   const isCartEmpty = checkout.lineItems.length === 0
 
-  console.log("Empty cart? " + isCartEmpty)
-
-  console.log("checkout:")
-  console.log(checkout)
-  console.log("checkout.lineItems[" + checkout.lineItems.length + "]: ")
-  let count = 0
-  checkout.lineItems.forEach(item => {
-    count++
-    console.log("Item " + count)
-    console.log(item)
-  })
+  //   console.log("checkout:")
+  //   console.log(checkout)
+  //   console.log("checkout.lineItems[" + checkout.lineItems.length + "]: ")
+  //   let count = 0
+  //   checkout.lineItems.forEach(item => {
+  //     count++
+  //     console.log("Item " + count)
+  //     console.log(item)
+  //   })
 
   const checkoutNow = () => {
     doCheckout()
