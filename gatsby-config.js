f189a371742535295b2b4b3f41475b6eeb463d71
CB <chrisrb83@gmail.com>
2021-10-27 22:40:30 -0400

    Install styled-components dependencies and use in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-styled-components`,
     // `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
